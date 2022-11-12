76ab0edae4b461361ecf946f62e249503960089d
unknown <huynhbangcong@gmail.com>
2022-11-12 22:36:04 +0700

    add vanilla extract

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { EsLinter, linterPlugin, TypeScriptLinter } from "vite-plugin-linter";
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+
 // https://vitejs.dev/config/
 
 export default defineConfig((configEnv) => ({
   plugins: [
     react(),
+    vanillaExtractPlugin({
+      identifiers: 'short'
+    }),
     linterPlugin({
       include: ["./src/**/*.ts", "./src/**/*.tsx"],
       linters: [new EsLinter({ configEnv: configEnv }), new TypeScriptLinter()],
