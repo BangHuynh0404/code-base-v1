641071c42bc675263b7d8114a7aeb5a2b99fd3e0
unknown <huynhbangcong@gmail.com>
2022-11-12 23:19:11 +0700

    remove eslint plugin && init storybook

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { EsLinter, linterPlugin, TypeScriptLinter } from "vite-plugin-linter";
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
-// https://vitejs.dev/config/
-
-export default defineConfig((configEnv) => ({
+export default defineConfig(({
   plugins: [
     react(),
     vanillaExtractPlugin({
       identifiers: 'short'
-    }),
-    linterPlugin({
-      include: ["./src/**/*.ts", "./src/**/*.tsx"],
-      linters: [new EsLinter({ configEnv: configEnv }), new TypeScriptLinter()],
     }),
   ],
 }));
