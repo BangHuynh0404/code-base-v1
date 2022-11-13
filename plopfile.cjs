e5de898841b3633ec1ae82e73b0da96e116b1b3a
unknown <huynhbangcong@gmail.com>
2022-11-13 16:56:18 +0700

    Modify eslint

--- plopfile.cjs
+++ plopfile.cjs
@@ -1,19 +1,19 @@
 /* eslint-disable no-undef */
 module.exports = (plop) => {
   // COMPONENT GENERATOR
-  plop.setGenerator("component", {
-    description: "Create a reusable component",
+  plop.setGenerator('component', {
+    description: 'Create a reusable component',
     prompts: [
       {
-        type: "list",
-        name: "type",
-        message: "Type of component?",
-        choices: () => ["atoms", "molecules", "organisms", "templates"],
+        type: 'list',
+        name: 'type',
+        message: 'Type of component?',
+        choices: () => ['atoms', 'molecules', 'organisms', 'templates'],
       },
       {
-        type: "input",
-        name: "name",
-        message: "Component name?",
+        type: 'input',
+        name: 'name',
+        message: 'Component name?',
       },
     ],
 
@@ -22,33 +22,33 @@ module.exports = (plop) => {
 
       actions.push(
         {
-          type: "add",
-          path: "src/components/{{type}}/{{pascalCase name}}/index.tsx",
-          templateFile: "src/plop-templates/component/index.tsx.hbs",
+          type: 'add',
+          path: 'src/components/{{type}}/{{pascalCase name}}/index.tsx',
+          templateFile: 'src/plop-templates/component/index.tsx.hbs',
         },
         {
-          type: "add",
-          path: "src/components/{{type}}/{{pascalCase name}}/index.stories.tsx",
-          templateFile: "src/plop-templates/component/index.stories.tsx.hbs",
+          type: 'add',
+          path: 'src/components/{{type}}/{{pascalCase name}}/index.stories.tsx',
+          templateFile: 'src/plop-templates/component/index.stories.tsx.hbs',
         },
         {
-          type: "add",
-          path: "src/components/{{type}}/{{pascalCase name}}/styles.ts",
-          templateFile: "src/plop-templates/component/styled.ts.hbs",
-        },
+          type: 'add',
+          path: 'src/components/{{type}}/{{pascalCase name}}/styles.ts',
+          templateFile: 'src/plop-templates/component/styled.ts.hbs',
+        }
       );
       return actions;
     },
   });
 
   // CONTAINER & PAGE
-  plop.setGenerator("Container", {
-    description: "Create a reusable component",
+  plop.setGenerator('Container', {
+    description: 'Create a reusable component',
     prompts: [
       {
-        type: "input",
-        name: "name",
-        message: "Container name?",
+        type: 'input',
+        name: 'name',
+        message: 'Container name?',
       },
     ],
     actions() {
@@ -56,27 +56,27 @@ module.exports = (plop) => {
 
       actions.push(
         {
-          type: "add",
-          path: "src/containers/{{pascalCase name}}/index.tsx",
-          templateFile: "src/plop-templates/container/index.tsx.hbs",
+          type: 'add',
+          path: 'src/containers/{{pascalCase name}}/index.tsx',
+          templateFile: 'src/plop-templates/container/index.tsx.hbs',
         },
         {
-          type: "add",
-          path: "src/containers/{{pascalCase name}}/types.ts",
-          templateFile: "src/plop-templates/container/types.ts.hbs",
-        },
+          type: 'add',
+          path: 'src/containers/{{pascalCase name}}/types.ts',
+          templateFile: 'src/plop-templates/container/types.ts.hbs',
+        }
       );
       return actions;
     },
   });
 
-  plop.setGenerator("Page", {
-    description: "Create a reusable component",
+  plop.setGenerator('Page', {
+    description: 'Create a reusable component',
     prompts: [
       {
-        type: "input",
-        name: "name",
-        message: "Pages name?",
+        type: 'input',
+        name: 'name',
+        message: 'Pages name?',
       },
     ],
     actions() {
@@ -84,30 +84,30 @@ module.exports = (plop) => {
 
       actions.push(
         {
-          type: "add",
-          path: "src/pages/{{pascalCase name}}/index.tsx",
-          templateFile: "src/plop-templates/page/index.tsx.hbs",
+          type: 'add',
+          path: 'src/pages/{{pascalCase name}}/index.tsx',
+          templateFile: 'src/plop-templates/page/index.tsx.hbs',
         },
         {
-          type: "add",
-          path: "src/pages/{{pascalCase name}}/styled.ts",
-          templateFile: "src/plop-templates/page/styled.ts.hbs",
-        },
+          type: 'add',
+          path: 'src/pages/{{pascalCase name}}/styled.ts',
+          templateFile: 'src/plop-templates/page/styled.ts.hbs',
+        }
       );
       return actions;
     },
   });
-  
-  plop.setHelper("classNameComponent", (type) => {
-    if (type === "atoms") {
-      return "a-";
+
+  plop.setHelper('classNameComponent', (type) => {
+    if (type === 'atoms') {
+      return 'a-';
     }
-    if (type === "molecules") {
-      return "m-";
+    if (type === 'molecules') {
+      return 'm-';
     }
-    if (type === "organisms") {
-      return "o-";
+    if (type === 'organisms') {
+      return 'o-';
     }
-    return "";
+    return '';
   });
 };
